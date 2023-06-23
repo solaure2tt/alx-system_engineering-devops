4aa5d17d71101b55cd44907615de5afacc454dae
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-23 11:59:56 -0700

    updt task 0 check 2 and 3

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Using Puppet, create a file school in /tmp.
-file { 'school' :
+
+file { 'school':
   path => '/tmp/school',
   mode => '0744',
   owner => 'www-data',
-  group => 'www-data'
-  content => 'I love Puppet'
+  group => 'www-data',
+  content => 'I love Puppet',
 }
