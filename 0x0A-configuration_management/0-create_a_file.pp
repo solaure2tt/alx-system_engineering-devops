3d7ea713a8ab1fab39374252021a52076fcd12d9
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-23 11:56:39 -0700

    task 0 to 2 update checks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Using Puppet, create a file school in /tmp.
-
-file { 'school':
+file { 'school' :
   path => '/tmp/school',
   mode => '0744',
   owner => 'www-data',
   group => 'www-data'
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process named killmenow
 exec { 'Kill a process killmenow',
   command => 'pkill -f killmenow',
-  provider => shell,
+  provider => shell
 }
